770aaa3198b8ef1190bfab953c7ec8a068bdd239
NIKITHA. M <61590715+NikithaM26@users.noreply.github.com>
2023-02-19 14:07:51 +0530

    edit: auth and slider

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -11,6 +11,8 @@ import iconPc from "../../assets/icons/PCs.svg";
 import iconMobile from "../../assets/icons/Mobile.svg";
 import iconWatches from "../../assets/icons/Watches-1.svg";
 import iconMouse from "../../assets/icons/Mouse.svg";
+import back from "../../assets/icons/Back.svg";
+import next from "../../assets/icons/Next.svg";
 
 import mobile from "../../assets/images/mobiles.png";
 import speaker from "../../assets/images/speaker.png";
@@ -26,175 +28,218 @@ import * as APINetwork from "../../utils/network";
 import devices from "../../contexts/devices.json";
 
 export default function Dashboard() {
-
-   //set state once devices received from api
-
+  const [minValue, setMinValue] = useState(0);
+  const [maxValue, setMaxValue] = useState(4);
+  const [authState, setAuthState] = useState();
   const [allDevices, setAllDevices] = useState([]);
 
-  async function fetchAllDevices () {
+  async function fetchAllDevices() {
     try {
       let devices = await APINetwork.getAllDevices();
-      if(devices ) {
+      if (devices) {
         setAllDevices(devices);
       }
-    }
-    catch(err) {
+    } catch (err) {
       console.error(err);
     }
   }
 
-  useEffect(() =>{
+  useEffect(() => {
+    const auth = () => {
+      try {
+        if (localStorage.getItem("token") && localStorage.getItem("user")) {
+          setAuthState(true);
+        } else {
+          setAuthState(false);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    auth();
+  }, []);
+
+  //set state once devices received from api
+
+  useEffect(() => {
     fetchAllDevices();
     console.log("Fetched all devices!");
     console.log(allDevices);
-  }, [allDevices])
- 
+  }, [allDevices]);
 
-  return (
-    <div className="dashboard">
-      <div class="side-menu">
-        <center>
-          <img src={logo} alt="Logo" />
-        </center>
+  function handleBack() {
+    setMinValue(minValue - 1 <= 0 ? 0 : minValue - 1);
+    setMaxValue(maxValue - 1 <= 4 ? 4 : maxValue - 1);
+  }
 
-        <nav>
-          <ul>
-            <li className="dashboard">
-              <a href="/dashboard">Dashboard</a>
-            </li>
-            <li className="logout">
-              <a href="/logout">Logout</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+  function handleNext() {
+    setMinValue(minValue + 4 === devices.length ? minValue : minValue + 1);
+    setMaxValue(maxValue + 1 >= devices.length ? devices.length : maxValue + 1);
+  }
 
-      <div className="main">
-        <h1>Dashboard</h1>
+  async function handleLogout() {
+    let response = await APINetwork.logout();
+    return response ? true : false;
+  }
 
-        <div>
-          <Stack direction="row" spacing={3}>
-            <Chip
-              avatar={<Avatar alt="all devices" src={iconAll} />}
-              label="All Devices"
-              variant="outlined"
-              component="a"
-              href="#all"
-              clickable
-              sx={{
-                "& .MuiChip-label": {
-                  color: "#FFFFFF",
-                },
-              }}
-            />
-            <Chip
-              avatar={<Avatar alt="mobile" src={iconMobile} />}
-              label="Mobile"
-              variant="outlined"
-              component="a"
-              href="#mobile"
-              clickable
-              sx={{
-                "& .MuiChip-label": {
-                  color: "#FFFFFF",
-                },
-              }}
-            />
-            <Chip
-              avatar={<Avatar alt="pcs" src={iconPc} />}
-              label="PCs"
-              variant="outlined"
-              component="a"
-              href="#pc"
-              clickable
-              sx={{
-                "& .MuiChip-label": {
-                  color: "#FFFFFF",
-                },
-              }}
-            />
-            <Chip
-              avatar={<Avatar alt="watches" src={iconWatches} />}
-              label="Watches"
-              variant="outlined"
-              component="a"
-              href="#watch"
-              clickable
-              sx={{
-                "& .MuiChip-label": {
-                  color: "#FFFFFF",
-                },
-              }}
-            />
-            <Chip
-              avatar={<Avatar alt="mouse" src={iconMouse} />}
-              label="Mouse"
-              sx={{
-                "& .MuiChip-label": {
-                  color: "#FFFFFF",
-                },
-              }}
-              variant="outlined"
-              component="a"
-              href="#mouse"
-              clickable
-            />
-          </Stack>
+  if (!authState || authState === undefined) {
+    return null;
+  } else {
+    return (
+      <div className="dashboard">
+        <div class="side-menu">
+          <center>
+            <img src={logo} alt="Logo" />
+          </center>
+
+          <nav>
+            <ul>
+              <li className="dashboard">
+                <a href="/dashboard">Dashboard</a>
+              </li>
+              <li className="logout">
+                <a href="/" onClick={handleLogout}>
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
 
-        <div class="card-group">
-          {devices.map((ele, index) => {
-            if (
-              ele.deviceType.type === "IOS" ||
-              ele.deviceType.type === "ANDRIOD"
-            ) {
-              return (
-                <CustomCard
-                  img={mobile}
-                  device={ele.deviceNickname}
-                  height="150"
-                  alt="mobile img"
-                />
-              );
-            }
+        <div className="main">
+          <h1>Dashboard</h1>
 
-            if (ele.deviceType.type === "WEARABLE") {
-              return (
-                <CustomCard
-                  img={watch}
-                  device={ele.deviceNickname}
-                  height="150"
-                  alt="mobile img"
-                />
-              );
-            }
-            if (ele.deviceType.type === "AUDIO") {
-              return (
-                <CustomCard
-                  img={speaker}
-                  device={ele.deviceNickname}
-                  height="150"
-                  alt="mobile img"
-                />
-              );
-            }
+          <div>
+            <Stack direction="row" spacing={3}>
+              <Chip
+                avatar={<Avatar alt="all devices" src={iconAll} />}
+                label="All Devices"
+                variant="outlined"
+                component="a"
+                href="#all"
+                clickable
+                sx={{
+                  "& .MuiChip-label": {
+                    color: "#FFFFFF",
+                  },
+                }}
+              />
+              <Chip
+                avatar={<Avatar alt="mobile" src={iconMobile} />}
+                label="Mobile"
+                variant="outlined"
+                component="a"
+                href="#mobile"
+                clickable
+                sx={{
+                  "& .MuiChip-label": {
+                    color: "#FFFFFF",
+                  },
+                }}
+              />
+              <Chip
+                avatar={<Avatar alt="pcs" src={iconPc} />}
+                label="PCs"
+                variant="outlined"
+                component="a"
+                href="#pc"
+                clickable
+                sx={{
+                  "& .MuiChip-label": {
+                    color: "#FFFFFF",
+                  },
+                }}
+              />
+              <Chip
+                avatar={<Avatar alt="watches" src={iconWatches} />}
+                label="Watches"
+                variant="outlined"
+                component="a"
+                href="#watch"
+                clickable
+                sx={{
+                  "& .MuiChip-label": {
+                    color: "#FFFFFF",
+                  },
+                }}
+              />
+              <Chip
+                avatar={<Avatar alt="mouse" src={iconMouse} />}
+                label="Mouse"
+                sx={{
+                  "& .MuiChip-label": {
+                    color: "#FFFFFF",
+                  },
+                }}
+                variant="outlined"
+                component="a"
+                href="#mouse"
+                clickable
+              />
+            </Stack>
+          </div>
 
-            if (ele.deviceType.type === "MAC") {
-              return (
-                <CustomCard
-                  img={laptop}
-                  device={ele.deviceNickname}
-                  height="150"
-                  alt="mobile img"
-                />
-              );
-            }
-          })}
+          <div className="slider-controls">
+            <img src={back} alt="" onClick={handleBack} />
+            <img src={next} alt="" onClick={handleNext} />
+          </div>
+
+          <div class="card-group">
+            {devices.length > 0 &&
+              devices.slice(minValue, maxValue).map((ele, index) => {
+                if (
+                  ele.deviceType.type === "IOS" ||
+                  ele.deviceType.type === "ANDRIOD"
+                ) {
+                  return (
+                    <CustomCard
+                      img={mobile}
+                      device={ele.deviceNickname}
+                      height="150"
+                      alt="mobile img"
+                    />
+                  );
+                }
+
+                if (ele.deviceType.type === "WEARABLE") {
+                  return (
+                    <CustomCard
+                      img={watch}
+                      device={ele.deviceNickname}
+                      height="150"
+                      alt="mobile img"
+                    />
+                  );
+                }
+                if (ele.deviceType.type === "AUDIO") {
+                  return (
+                    <CustomCard
+                      img={speaker}
+                      device={ele.deviceNickname}
+                      height="150"
+                      alt="mobile img"
+                    />
+                  );
+                }
+
+                if (ele.deviceType.type === "MAC") {
+                  return (
+                    <CustomCard
+                      img={laptop}
+                      device={ele.deviceNickname}
+                      height="150"
+                      alt="mobile img"
+                    />
+                  );
+                }
+              })}
+          </div>
+
+          <h3>Track Your Devices</h3>
+          <Map />
         </div>
-
-        <h3>Track Your Devices</h3>
-        <Map />
       </div>
-    </div>
-  );
+    );
+  }
 }
